5f376c8ba71fd2bc2c136392d26174770d1fbc0a
CarlosMarinTC-alt <carlosmarintc@gmail.com>
2025-11-05 14:53:00 -0600

    Se agregaron las colisiones al barandal

--- Assets/Scripts/Cambiar_Posicion.cs
+++ Assets/Scripts/Cambiar_Posicion.cs
@@ -5,6 +5,7 @@ public class Cambiar_Posicion : MonoBehaviour
     public Vector2 Posicion_pasillo = new Vector2(8.92f, 0.55f);
     public Vector2 Posicion_habitacion = new Vector2(0.27f, 1.71f);
     public Vector2 Posicion_cama = new Vector2(5.35f, 0.52f);
+    public Vector2 Posicion_hab_tios = new Vector2(18.25f, 0.82f);
 
     void Start()
     {
@@ -24,6 +25,9 @@ public class Cambiar_Posicion : MonoBehaviour
                     break;
                 case 3:
                     protaTransform.position = Posicion_pasillo;
+                    break;
+                case 4:
+                    protaTransform.position = Posicion_hab_tios;
                     break;
                 case 99:
                     protaTransform.position = Posicion_cama;
